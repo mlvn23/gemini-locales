e1732c3f88c7218fd34697a9b9a97967d000d925
Melvin Lim <mlim@extrategic.com>
2020-03-17 21:18:10 -0700

    Added DateTimeFormatter test

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,24 @@
 package test
 
-//import java.time.format.DateTimeFormatter
+import java.time._
+import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 object App {
     def main(args: Array[String]): Unit = {
     
-        println(s"The default locale is=|${Locale.getDefault}| lang=|${Locale.getDefault.getLanguage}|")
+       def test(title: String): Unit = {
+          val MONTH_FMT = DateTimeFormatter.ofPattern("MMM yyy")
+          println(s"${title} locale is=|${Locale.getDefault}| lang=|${Locale.getDefault.getLanguage}| MONTH=${LocalDate.of(2019, 11, 1).format(MONTH_FMT)}")
+       }
+       
+       test("default")
     
-        Locale.setDefault(Locale.US)    
-        println(s"US locale is=|${Locale.getDefault}| lang=|${Locale.getDefault.getLanguage}|")
+       Locale.setDefault(Locale.US)
+       test("US")
         
-        Locale.setDefault(Locale.ENGLISH)    
-        println(s"EN locale is=|${Locale.getDefault}| lang=|${Locale.getDefault.getLanguage}|")
+       Locale.setDefault(Locale.ENGLISH)
+       test("ENGLISH")        
         
     }
 }
